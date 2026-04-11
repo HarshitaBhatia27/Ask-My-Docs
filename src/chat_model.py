3ebc30fe6aee95c002ecee99781d0577b40a10fa
Harshita Bhatia <harshitabhatia272002@gmail.com>
2026-04-11 17:10:51 +0530

    Your branch is up to date with 'origin/main'.

--- src/chat_model.py
+++ src/chat_model.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 
-load_dotenv()
-
 def get_llm():
-    return ChatGroq(model="llama-3.3-70b-versatile")
+    api_key = os.environ.get("GROQ_API_KEY")
+    return ChatGroq(model="llama-3.3-70b-versatile", groq_api_key=api_key)
 
 def get_embeddings():
     return HuggingFaceEmbeddings(model_name="BAAI/bge-small-en")
